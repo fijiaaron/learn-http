b3c5d534c108e2df8a2c79e98ab542f974cf3022
Aaron Evans <aaron.evans@diio.net>
2015-03-05 13:48:03 -0700

    Create a simple test opening and closing a socket connection

---
+++ src/test/java/fijiaaron/learnHttp/SocketConnectionTest.java
@@ -0,0 +1,28 @@
+package fijiaaron.learnHttp;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.junit.Test;
+
+public class SocketConnectionTest {
+
+    String hostname = "example.com";
+    int port = 80;
+
+    @Test
+    public void testSocketConnection() throws IOException {
+        Socket socket = new Socket();
+        InetSocketAddress address = new InetSocketAddress(hostname, port);
+        socket.connect(address);
+
+        assertTrue(socket.isConnected());
+
+        socket.close();
+
+        assertTrue(socket.isClosed());
+    }
+}
